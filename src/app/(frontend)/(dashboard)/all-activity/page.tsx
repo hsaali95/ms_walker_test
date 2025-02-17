bcb78aefcbf2960c6e199c5c025f318542647f86
Ahmed Ali <ahmed.ali@softvira.com>
2025-02-17 12:01:06 +0500

    fix

--- src/app/(frontend)/(dashboard)/all-activity/page.tsx
+++ src/app/(frontend)/(dashboard)/all-activity/page.tsx
@@ -3,12 +3,18 @@ import React, { useEffect } from "react";
 import ActivityTable from "../../features/show-all-activity/activity-table";
 import { useAppDispatch } from "../../hooks/redux-hook";
 import { setActivityCsvIdle } from "../../redux/slices/activity/download-activity-csv-file";
+import {
+  clearActivityTableData,
+  clearAllActivityFields,
+} from "../../redux/slices/activity/get-activity-slice";
 
 const AllActivity = () => {
   const dispatch = useAppDispatch();
   useEffect(() => {
     return () => {
       dispatch(setActivityCsvIdle());
+      dispatch(clearAllActivityFields());
+      dispatch(clearActivityTableData());
     };
   }, []);
   return (
