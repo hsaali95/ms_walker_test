92eebece526f1a85993bb1adc2ab74b030315790
Mustafa Mirza <mustafa.mirza@softvira.com>
2025-03-12 12:39:30 +0500

    accounts inported

--- src/utils/transformer.ts
+++ src/utils/transformer.ts
@@ -114,3 +114,148 @@ export const transformSurvey = async (surveyData: SurveyDetail) => {
     created_at: surveyData.Date,
   } as unknown as Survey;
 };
+
+export type TCustomers = {
+  "LOC #": number;
+  "CUST NO ": number;
+  "CUSTOMER NAME": string;
+  ADDRESS1: string;
+  ADDRESS2: string;
+  CITY: string;
+  STATE: string;
+  ZIP: number;
+  "ZIP+4": number;
+  TELEPHONE: string;
+  "CUSTOMER CONTACT": string;
+  "MAIL-TO": number;
+  "A B C  NO ": number;
+  "CMP-Cd": number;
+  TXCHEK: number;
+  "L-SLSMAN #": number;
+  "W-SLSMAN #": number;
+  "S-SLSMAN #": number;
+  "H-SLSMAN #": number;
+  "M-SLSMAN #": number;
+  TERRITORY: number;
+  ACCUM: "Yes" | "No";
+  "CREDIT CODE": number;
+  "CREDIT LIMIT": number;
+  "CASH DISC": "Y" | "N";
+  PCD: number;
+  DCD: string;
+  TYPE: number;
+  "ROUTE NO ": number;
+  "STOP NO ": number;
+  "STOP 2 ": number;
+  "DEL  DAY": string;
+  "DAYS PAST DUE": number;
+  "CREDIT RESTRICT": "Y" | "N";
+  "COMBINE INVOICE": "Y" | "N";
+  "B O FILL": "Y" | "N";
+  "WINE NALC ONLY": "Y" | "N";
+  "FAX PHONE#": number;
+  EMAIL: string;
+  "TAX ID": string;
+  "SECOND COPY INV": "Y" | "N";
+  "STATEMENT CODE": string;
+  "STATEMENT EMAIL": string;
+  "ALTERNATE CARRIER": "Y" | "N";
+  "ALLOW SUB ITEM": "Y" | "N";
+  "CONTACT TITLE": string;
+  "CONTACT NAME": string;
+  "CONTACT EMAIL": string;
+  "PHONE NUMBER": string;
+  "MOBILE NUMBER": number;
+  "SHIPPING INSTRUCTIONS": string;
+  "DELIVERY INSTRUCTIONS": string;
+  "C O D  DESC": string;
+  COMMISSION: string;
+  CHAIN: string;
+  "INACTIVE FLAG": string;
+  SUB: string;
+  "BAILMENT CODE": string;
+  "PRINT SCC": string;
+  "COUNTY CODE": string;
+  "CITY TAX": string;
+  "CONTACT 2": string;
+  "WEB URL": string;
+  "TRADING PARTNER": string;
+  "TRADING PART STORE": string;
+  "PHONE EXT": string;
+  "JOB TITLE": string;
+  "DEL DAY": string;
+};
+export const transformAccount = (customer: TCustomers) => {
+  return {
+    locNumber: customer["LOC #"],
+    custNumber: customer["CUST NO "],
+    customerName: customer["CUSTOMER NAME"],
+    address1: customer["ADDRESS1"],
+    address2: customer["ADDRESS2"],
+    city: customer["CITY"],
+    state: customer["STATE"],
+    zip: customer["ZIP"],
+    zipPlus4: customer["ZIP+4"],
+    telephone: customer["TELEPHONE"],
+    customerContact: customer["CUSTOMER CONTACT"],
+    mailTo: customer["MAIL-TO"],
+    abcNumber: customer["A B C  NO "],
+    cmpCd: customer["CMP-Cd"],
+    commission: customer["COMMISSION"],
+    txChek: customer["TXCHEK"],
+    lSlsmanNumber: customer["L-SLSMAN #"],
+    wSlsmanNumber: customer["W-SLSMAN #"],
+    sSlsmanNumber: customer["S-SLSMAN #"],
+    hSlsmanNumber: customer["H-SLSMAN #"],
+    mSlsmanNumber: customer["M-SLSMAN #"],
+    territory: customer["TERRITORY"],
+    chain: customer["CHAIN"],
+    accum: customer["ACCUM"],
+    creditCode: customer["CREDIT CODE"],
+    creditLimit: customer["CREDIT LIMIT"],
+    cashDisc: customer["CASH DISC"],
+    pcd: customer["PCD"],
+    dcd: customer["DCD"],
+    type: customer["TYPE"],
+    sub: customer["SUB"],
+    routeNumber: customer["ROUTE NO "],
+    stopNumber: customer["STOP NO "],
+    stop2: customer["STOP 2 "],
+    delDay: customer["DEL  DAY"],
+    shippingInstructions: customer["SHIPPING INSTRUCTIONS"],
+    deliveryInstructions: customer["DELIVERY INSTRUCTIONS"],
+    inactiveFlag: customer["INACTIVE FLAG"],
+    codDesc: customer["C O D  DESC"],
+    daysPastDue: customer["DAYS PAST DUE"],
+    creditRestrict: customer["CREDIT RESTRICT"],
+    combineInvoice: customer["COMBINE INVOICE"],
+    bailmentCode: customer["BAILMENT CODE"],
+    bofill: customer["B O FILL"],
+    wineNalcOnly: customer["WINE NALC ONLY"],
+    printScc: customer["PRINT SCC"],
+    countyCode: customer["COUNTY CODE"],
+    cityTax: customer["CITY TAX"],
+    contact2: customer["CONTACT 2"],
+    faxPhone: customer["FAX PHONE#"],
+    email: customer["EMAIL"],
+    webUrl: customer["WEB URL"],
+    taxId: customer["TAX ID"],
+    secondCopyInv: customer["SECOND COPY INV"],
+    tradingPartner: customer["TRADING PARTNER"],
+    tradingPartStore: customer["TRADING PART STORE"],
+    statementCode: customer["STATEMENT CODE"],
+    statementEmail: customer["STATEMENT EMAIL"],
+    alternateCarrier: customer["ALTERNATE CARRIER"],
+    allowSubItem: customer["ALLOW SUB ITEM"],
+    contactTitle: customer["CONTACT TITLE"],
+    contactName: customer["CONTACT NAME"],
+    contactEmail: customer["CONTACT EMAIL"],
+    phoneNumber: customer["PHONE NUMBER"],
+    mobileNumber: customer["MOBILE NUMBER"],
+    phoneExt: customer["PHONE EXT"],
+    jobTitle: customer["JOB TITLE"],
+    dellDay: customer["DEL  DAY"],
+    isNew: true,
+    fullCustomerInfo: customer["CUSTOMER NAME"] + " - " + customer["CUST NO "],
+  };
+};
