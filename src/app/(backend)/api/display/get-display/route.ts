3333b0e5d06ce4189b88c62a0363932b7033d532
Mustafa Mirza <mustafa.mirza@softvira.com>
2025-04-04 11:30:47 +0500

    get display based on the group of the user

--- src/app/(backend)/api/display/get-display/route.ts
+++ src/app/(backend)/api/display/get-display/route.ts
@@ -1,12 +1,81 @@
+import AccessType from "@/db/models/access-type";
 import Display from "@/db/models/display";
+import Group from "@/db/models/group";
+import GroupMembers from "@/db/models/group-members";
+import JWTService from "@/services/jwt/jwt-services";
 import { errorResponse, successResponse } from "@/utils/response.decorator";
+import { NextRequest } from "next/server";
+import { Op, Sequelize } from "sequelize";
 export const dynamic = "force-dynamic"; // ✅ Forces API to fetch fresh data on every request
 
 // Handle GET requests to fetch all display records
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const cookieHeader = request.headers.get("cookie");
   try {
     // Fetch all displays from the database
-    const displays = await Display.findAll();
+
+    // Retrieve cookies from the request headers
+    let token: string | null = null;
+
+    if (cookieHeader) {
+      const cookies = Object.fromEntries(
+        cookieHeader.split("; ")?.map((cookie) => {
+          const [key, value] = cookie.split("=");
+          return [key, value];
+        })
+      );
+      token = cookies["session_accessToken"];
+    }
+    const userIdForAccessTypes: any = await JWTService.verifyAccessToken(
+      token || ""
+    );
+
+    const userAssignedGroups = (await Group.findAll({
+      attributes: [
+        [
+          Sequelize.literal('array_agg(DISTINCT "access_type"."id")'),
+          "group_access_type_id",
+        ],
+      ],
+      where: {
+        is_active: true,
+      },
+      include: [
+        {
+          model: AccessType,
+          as: "access_type",
+          attributes: [],
+          where: {
+            name: {
+              [Op.in]: ["Sales", "Merchandiser"],
+            },
+          },
+        },
+        {
+          model: GroupMembers,
+          as: "group_members",
+          where: {
+            user_id: userIdForAccessTypes.id,
+          },
+          attributes: [],
+        },
+      ],
+      raw: true,
+    })) as unknown as any;
+    
+    // eslint-disable-next-line
+    let queryOptions: { [key: string]: any } = {};
+
+    const accessTypes = userAssignedGroups[0].group_access_type_id;
+    if (accessTypes && accessTypes.length) {
+      queryOptions.where = {
+        group_access_type_id: {
+          [Op.in]: accessTypes,
+        },
+      };
+    }
+
+    const displays = await Display.findAll(queryOptions);
 
     // Check if any displays are found
     if (displays.length === 0) {
